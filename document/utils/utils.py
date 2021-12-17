5b3e5dcc8b39fa43a8ca78717541b21dfd22ae0f
zchmielewska@gmail.com <zchmielewska@gmail.com>
2021-12-17 22:28:22 +0100

    show history

--- document/utils/utils.py
+++ document/utils/utils.py
@@ -2,14 +2,14 @@ from document import models
 
 
 def search(phrase):
-    doc_product_name = models.Document.objects.filter(product__name__icontains=phrase)
-    doc_product_model = models.Document.objects.filter(product__model__icontains=phrase)
-    doc_category_name = models.Document.objects.filter(category__name__icontains=phrase)
-    doc_validity_start = models.Document.objects.filter(validity_start__icontains=phrase)
-    doc_file_name = models.Document.objects.filter(file__icontains=phrase)
-    doc_created_by = models.Document.objects.filter(created_by__username__icontains=phrase)
-    doc_created_at = models.Document.objects.filter(created_at__icontains=phrase)
-    all_documents = doc_product_name | doc_product_model | doc_category_name | doc_validity_start | doc_file_name \
-                    | doc_created_by | doc_created_at
+    d1 = models.Document.objects.filter(id__icontains=phrase)
+    d2 = models.Document.objects.filter(product__name__icontains=phrase)
+    d3 = models.Document.objects.filter(product__model__icontains=phrase)
+    d4 = models.Document.objects.filter(category__name__icontains=phrase)
+    d5 = models.Document.objects.filter(validity_start__icontains=phrase)
+    d6 = models.Document.objects.filter(file__icontains=phrase)
+    d7 = models.Document.objects.filter(created_by__username__icontains=phrase)
+    d8 = models.Document.objects.filter(created_at__icontains=phrase)
+    all_documents = d1 | d2 | d3 | d4 | d5 | d6 | d7 | d8
     documents = all_documents.distinct().order_by("-id")
     return documents
