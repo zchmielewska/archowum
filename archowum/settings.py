c1f385c19a4acafcfe7dafa5b9f99576ceaf9bd1
zchmielewska@gmail.com <zchmielewska@gmail.com>
2022-01-04 21:43:17 +0100

    add default value for bucket name

--- archowum/settings.py
+++ archowum/settings.py
@@ -166,7 +166,7 @@ LOGIN_URL = "/login/"
 # AWS S3 bucket
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "")
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3StaticStorage"
